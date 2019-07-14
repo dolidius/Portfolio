c688fa8617d699ec77858c4543030ba70b8492c0
dolidius <dolidius@gmail.com>
2019-07-14 12:36:30 +0200

    Added tft compedium to projects

--- components/Portfolio/Portfolio.js
+++ components/Portfolio/Portfolio.js
@@ -40,12 +40,13 @@ const Portfolio = () => (
         />
 
         <PortfolioItem
-            title="Hangman Game"
-            imageName="hangman.png"
-            liveHref="https://condescending-feynman-2e8df7.netlify.com/"
-            codeHref="https://github.com/dolidius/Hangman-Game"
-            tags={["HTML5", "CSS", "Javascript"]}
-            desc="Hangman is a guessing game. Computer choose a random word, phrase or sentence related to programming and the player tries to guess it by suggesting letters, within a certain number of guesses."
+            title="TFT Compedium"
+            imageName="tft.png"
+            liveHref="https://play.google.com/store/apps/details?id=com.stranweb.TeamfightTacticsCompedium"
+            // codeHref="https://github.com/dolidius/Hangman-Game"
+            tags={["React", "React Native", "Javascript", "React Navigation"]}
+            // desc="Hangman is a guessing game. Computer choose a random word, phrase or sentence related to programming and the player tries to guess it by suggesting letters, within a certain number of guesses."
+            desc="Teamfight tactics compedoum is a wiki app to look up and learn about Teamfight Tactics game."
         />
 
     </section>
